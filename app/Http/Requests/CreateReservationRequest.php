f0a535b21fd0c9f37ae9c3b3b1ba2a194171b9cb
Ahmed Hamada <ahmedhamada015@gmail.com>
2025-05-05 20:18:55 +0300

    validate on double booking

--- app/Http/Requests/CreateReservationRequest.php
+++ app/Http/Requests/CreateReservationRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Reservation;
+use Illuminate\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CreateReservationRequest extends FormRequest
@@ -25,4 +27,21 @@ class CreateReservationRequest extends FormRequest
             'reservation_date' => ['required','date','after:now'],
         ];
     }
+
+    public function withValidator(Validator $validator)
+{
+    $validator->after(function ($validator) {
+        $userId = auth()->id(); // or $this->user()->id if applicable
+        $reservationDate = $this->input('reservation_date');
+
+        $exists = Reservation::where('user_id', $userId)
+            ->where('reservation_date', $reservationDate)
+            ->exists();
+
+        if ($exists) {
+            $validator->errors()->add('reservation_date', 'You already have a reservation at this date and time.');
+        }
+    });
+}
+
 }
